bdce8009105f09d933bcf8f340ebab5f01574419
YellowFlash1901 <vardaansathe@gmial.com>
2022-03-02 23:53:49 +0530

    Day 2

---
+++ constants.py
@@ -0,0 +1,14 @@
+# PARAMETERS
+
+LINE_WIDTH = 15
+
+#Colors
+
+BG_COLOR = (0,0,0)
+LINE_COLOR = (255,255,255)
+
+#adding rows and coloumn and square size
+ROWS =  3
+COL = 3
+
+SQSIZE = 600 // COL
--- tictactoe.py
+++ tictactoe.py
@@ -5,21 +5,35 @@ import pygame
 
 from constants import *
 
-#pygame
+#pygame SETUP
 
 pygame.init()
 screen = pygame.display.set_mode((600,600))
 pygame.display.set_caption("TIC TAC TOE AI By vardaan sathe")
+screen.fill(BG_COLOR)  #Background color
 
+#creating game object 
+
+class Game:
+    
+    def __init__(self):
+        self.show_lines()
+
+    def show_lines(self):
+        #VERTICAL
+        #Format of line fuctoin is (screen , line color , starting position , ending position , width )
+        pygame.draw.line(screen,LINE_COLOR,(SQSIZE,0),(SQSIZE,600), LINE_WIDTH)
 
 def main():
-    
+    #object
+    game = Game()
     while True:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-
+    
+    Pygame.display.update() #updates the Pygame setup
 
 main()
